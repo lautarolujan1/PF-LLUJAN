f3ce06c9b72b1f42d92977c8aa0cb47063de0c27
lautarolujan <lautarolujan199@gmail.com>
2023-10-02 19:06:44 -0300

    pre-entrega2

--- curso.js
+++ curso.js
@@ -1,42 +1,48 @@
 
-let total = 0;
-const productos = [];
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+class CarritoDeCompras {
+    constructor() {
+        this.productos = [];
+    }
+
+    agregarProducto(nombre, precio) {
+        if (!isNaN(precio)) {
+            const producto = new Producto(nombre, precio);
+            this.productos.push(producto);
+            this.actualizarListaProductos();
+            console.log(`Producto agregado: ${producto.nombre} - $${producto.precio}`);
+        } else {
+            alert("El precio ingresado no es válido.");
+        }
+    }
+
+    actualizarListaProductos() {
+        const productosDiv = document.getElementById("productos");
+        productosDiv.innerHTML = "";
+        this.productos.forEach(producto => {
+            productosDiv.innerHTML += `<p>${producto.nombre}: $${producto.precio}</p>`;
+        });
+    }
+
+    calcularTotal() {
+        const total = this.productos.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+        document.getElementById("total").textContent = total.toFixed(2);
+        console.log(`Total calculado: $${total.toFixed(2)}`);
+    }
+}
+
+const carrito = new CarritoDeCompras();
 
 function agregarProducto() {
     const nombre = prompt("Ingrese el nombre del producto:");
     if (nombre !== null && nombre !== "") {
         const precio = parseFloat(prompt("Ingrese el precio del producto:"));
-        if (!isNaN(precio)) {
-            productos.push({ nombre, precio });
-            actualizarListaProductos();
-            console.log("Producto agregado: " + nombre + " - $" + precio);
-        } else {
-            alert("El precio ingresado no es válido.");
-        }
+        carrito.agregarProducto(nombre, precio);
     }
-}
-
-function actualizarListaProductos() {
-   const productosDiv = document.getElementById("productos");
-   productosDiv.innerHTML = "";
-   let indice = 0;
-
-   while (indice < productos.length) {
-       const producto = productos[indice];
-       productosDiv.innerHTML += `<p>${producto.nombre}: $${producto.precio}</p>`;
-       indice++;
-   }
-}
-
-function calcularTotal() {
-   total = 0;
-   let indice = 0;
-
-   while (indice < productos.length) {
-       total += productos[indice].precio;
-       indice++;
-   }
-
-   document.getElementById("total").textContent = total.toFixed(2);
-   console.log("Total calculado: $" + total.toFixed(2));
 }
